9ae46457a78e108da49ee17f3b89dba5f6a998d8
chenfeng <chenfeng@iastate.edu>
2016-11-28 19:56:39 -0600

    Modified Drawing Function/Activity

--- RealTimeSimApp/app/src/main/java/chenfeng/realtimesimapp/timeScrollingActivity.java
+++ RealTimeSimApp/app/src/main/java/chenfeng/realtimesimapp/timeScrollingActivity.java
@@ -11,9 +11,8 @@ import android.support.v7.app.AppCompatActivity;
 
 import android.view.Display;
 import android.view.View;
-import android.widget.HorizontalScrollView;
+
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -45,37 +44,44 @@ public class timeScrollingActivity extends AppCompatActivity {
         private ArrayList<Integer> startTime, endTime, taskId, colors;
         private ArrayList<Rect> mRectangles;
 
-        private int SCALE_INDEX = 15;
-        private int RECT_HEIGHT = 50;
+        private int SCALE_INDEX = 100;
+        private int RECT_HEIGHT = 100;
         private int Y_RMS = 100, Y_EDF = 400, Y_LLF = 700;
+        private int X_PADDING = 200;
 
 
         public CustomView(Context context) {
             super(context);
+
+            mPaint = new Paint();
+            mPaint.setColor(Color.GRAY);
+
             clearLists();
-            new scheduleRMS();
-            numTasks = scheduleRMS.getNumTasks();
-            startTime = scheduleRMS.getStartList();
-            endTime = scheduleRMS.getEndList();
-            taskId = scheduleRMS.getIdList();
+
+            scheduleRMS mRMSScheduler = new scheduleRMS();
+            numTasks = mRMSScheduler.getNumTasks();
+            startTime = mRMSScheduler.getStartList();
+            endTime = mRMSScheduler.getEndList();
+            taskId = mRMSScheduler.getIdList();
+
 
             getRandomColor();
             generateRectList();
 
-            mPaint = new Paint();
-            mPaint.setColor(Color.LTGRAY);
+
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
+            //set background color
+            canvas.drawColor(Color.LTGRAY);
+            //Draw RMS schedule
+            drawRMSSchedule(canvas);
+            //Draw EDF schedule
+            drawEDFSchedule(canvas);
+            //Draw LLF schedule
+            drawLLFSchedule(canvas);
 
-            canvas.drawColor(Color.CYAN);
-            canvas.drawText("RMS", 0, 3, 20.0f, 50.0f, mPaint);
-
-            for(int i = 0; i< taskId.size(); i++){
-                mPaint.setColor(colors.get(taskId.get(i)));
-                canvas.drawRect(mRectangles.get(i), mPaint);
-            }
 
         }
 
@@ -83,27 +89,54 @@ public class timeScrollingActivity extends AppCompatActivity {
         protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
             Display d = getWindowManager().getDefaultDisplay();
             Point dSize = new Point();
+
             d.getSize(dSize);
+
             int width = dSize.x + 1000;
             int height = dSize.y;
             setMeasuredDimension(width, height);
 
         }
+
         ////////////////////////////////////////////////////////////////////////////////////////////
         //draw helper function
         ////////////////////////////////////////////////////////////////////////////////////////////
-        private void clearLists(){
-            startTime = new ArrayList<>();
-            endTime = new ArrayList<>();
-            taskId = new ArrayList<>();
-            colors = new ArrayList<>();
-            mRectangles = new ArrayList<>();
+        private void drawRMSSchedule(Canvas canvas) {
+
+            for (int i = 0; i < taskId.size(); i++) {
+                mPaint.setColor(colors.get(taskId.get(i)));
+                canvas.drawRect(mRectangles.get(i), mPaint);
+            }
+            mPaint.setColor(Color.BLACK);
+            mPaint.setStrokeWidth(10.0f);
+
+            float lineWidth = (float) endTime.get(endTime.size()-1) * SCALE_INDEX;
+            canvas.drawLine(150.0f, (float) Y_RMS, (250.0f + lineWidth) , (float) Y_RMS, mPaint);
+        }
+
+        private void drawEDFSchedule(Canvas canvas) {
+
+        }
+
+        private void drawLLFSchedule(Canvas canvas) {
+
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        //List helper function
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        private void clearLists() {
+            startTime = new ArrayList<Integer>();
+            endTime = new ArrayList<Integer>();
+            taskId = new ArrayList<Integer>();
+            colors = new ArrayList<Integer>();
+            mRectangles = new ArrayList<Rect>();
         }
 
         private void getRandomColor() {
             Random ran = new Random();
 
-            for(int i = 0; i< numTasks; i++){
+            for (int i = 0; i < numTasks; i++) {
                 int randColor = Color.argb(255, ran.nextInt(256), ran.nextInt(256), ran.nextInt(256));
                 colors.add(randColor);
             }
@@ -112,9 +145,9 @@ public class timeScrollingActivity extends AppCompatActivity {
         private void generateRectList() {
 
             for (int i = 0; i < startTime.size(); i++) {
-                int x = startTime.get(i);
-                int width = endTime.get(i);
-                mRectangle = new Rect(x * SCALE_INDEX, Y_RMS, width * SCALE_INDEX, RECT_HEIGHT);
+                int x = startTime.get(i) * SCALE_INDEX;
+                int width = (endTime.get(i) - startTime.get(i)) * SCALE_INDEX;
+                mRectangle = new Rect((x + X_PADDING), Y_RMS, (width + x + X_PADDING), (RECT_HEIGHT + Y_RMS));
                 mRectangles.add(mRectangle);
             }
 
