251f270d0a4e3e5a11162b44e52e662b6679a33e
Yuxiang Chen <yuxiangc@iastate.edu>
2016-11-29 18:50:13 -0600

    Merge branches 'chenfeng' and 'yuxiang' of github.com:ChenfengLiu/RealTimeTaskSimApp into yuxiang
    # Conflicts:
    #	RealTimeSimApp/app/src/main/java/chenfeng/realtimesimapp/model/ScheduleRMS.java

--- RealTimeSimApp/app/src/main/java/chenfeng/realtimesimapp/model/ScheduleRMS.java
+++ RealTimeSimApp/app/src/main/java/chenfeng/realtimesimapp/model/ScheduleRMS.java
@@ -1,83 +1,201 @@
 package chenfeng.realtimesimapp.model;
 
 import java.util.ArrayList;
-
-import static android.R.attr.id;
+import java.util.Collections;
 
 /**
  * Created by chenfeng on 22/11/16.
  */
 public class ScheduleRMS {
 
-    //program assigned & generated
-    private int taskInstance, startTime, endTime;
+    private ArrayList<Task> tListInput;
+    private int simTime;
 
-    private int numTasks;
-    private ArrayList<Integer> startTimeList, endTimeList, taskIdList;
+    private ArrayList<Instance> tListProcessed;
 
-    public ScheduleRMS() {
-        numTasks = 3;
-        startTimeList = new ArrayList<>();
-        startTimeList.add(0);
-        startTimeList.add(1);
-        startTimeList.add(3);
-        endTimeList = new ArrayList<>();
-        endTimeList.add(1);
-        endTimeList.add(2);
-        endTimeList.add(6);
-        taskIdList = new ArrayList<>();
-        taskIdList.add(0);
-        taskIdList.add(1);
-        taskIdList.add(2);
+    private int[] idArr, startTimeArr, endTimeArr, instanceArr;
 
+    public ScheduleRMS(ArrayList<Task> tasks) {
+        //calculate simulation time
+        tListInput = tasks;
+        simTime = simTime(tasks, 0, tasks.size());
+
+        System.out.println("input is: " + tListInput.get(1).getId() + "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        idArr = new int[simTime];
+        startTimeArr = new int[simTime];
+        endTimeArr = new int[simTime];
+        instanceArr = new int[simTime];
+
+        tListProcessed = new ArrayList<>();
+        runScheduler(tListInput);
 
     }
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    //Task ArrayList Functions
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-    public int getNumTasks() {
-        return numTasks;
-    }
 
-    public ArrayList<Integer> getStartList() {
-        return startTimeList;
-    }
-
-    public ArrayList<Integer> getEndList() {
-        return endTimeList;
-    }
-
-    public ArrayList<Integer> getIdList() {
-        return taskIdList;
-    }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
-    //Getters and Setters
+    //EDF Scheduler helper functions
     ////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-    public void setTaskInstance(int i) {
-        this.taskInstance = i;
+    public void runScheduler(ArrayList<Task> tasks) {
+        if (isSchedulable(tasks)) {
+            //create all task instance with different arrive times
+            createInstance(tListInput);
+            simulate(tListProcessed, simTime);
+        } else {
+            idArr = null;
+        }
     }
 
-    public int getTaskInstance() {
-        return taskInstance;
+    private void simulate(ArrayList<Instance> instances, int sTime) {
+        ArrayList<Integer> indexList;
+
+        for (int i = 0; i < sTime; i++) {
+            //find all ready tasks at current time
+            indexList = findReadyTasks(i, instances);
+
+            //choose one instance to execute
+            int chosen = chooseInstance(i, indexList);
+
+            //execute the instance
+            runInstance(i, chosen);
+
+        }
+
     }
 
-    public void setStartTime(int s) {
-        this.startTime = s;
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //Simulation helper functions
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    private void createInstance(ArrayList<Task> tasks) {
+
+        for (Task t : tasks) {
+            int numInstance = simTime / t.getP();
+            for (int i = 0; i < numInstance; i++) {
+                Instance mInstance = new Instance(t.getId(), i * t.getP(), t.getC(), t.getP(), i);
+                System.out.println("Task ID: " + mInstance.getId() + "; Task Instance: " + mInstance.getInstance() + "!!!!!!!!!!!!!!!");
+                tListProcessed.add(mInstance);
+            }
+        }
     }
 
-    public int getStartTime() {
-        return startTime;
+    private ArrayList<Integer> findReadyTasks(int currentTime, ArrayList<Instance> instances) {
+        ArrayList<Integer> readyInstanceIndex = new ArrayList<>();
+        for (int i = 0; i < instances.size(); i++) {
+            //if arrived, and not finished execution
+            if (instances.get(i).getArriveTime() <= currentTime && instances.get(i).getC() > 0) {
+                readyInstanceIndex.add(i);
+            }
+        }
+        return readyInstanceIndex;
     }
 
-    public void setEndTime(int e) {
-        this.endTime = id;
+    private int chooseInstance(int currentTime, ArrayList<Integer> index) {
+        int target = -1;
+        int min = Integer.MAX_VALUE;
+        for (int i : index) {
+            if ((tListProcessed.get(i).getP()) < min) {
+                min = tListProcessed.get(i).getP();
+                target = i;
+            }
+        }
+        return target;
     }
 
-    public int getEndTime() {
-        return endTime;
+    private void runInstance(int currentTime, int index) {
+        if (index == -1) {
+            idArr[currentTime] = -1;
+            startTimeArr[currentTime] = -1;
+            endTimeArr[currentTime] = -1;
+            instanceArr[currentTime] = -1;
+        } else {
+            idArr[currentTime] = tListProcessed.get(index).getId();
+            startTimeArr[currentTime] = currentTime;
+            endTimeArr[currentTime] = currentTime + 1;
+            instanceArr[currentTime] = tListProcessed.get(index).getInstance();
+
+            tListProcessed.get(index).executeC();
+        }
+    }
+
+    private Boolean isSchedulable(ArrayList<Task> tasks) {
+        sort(tasks);
+        return exactTest(tasks);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //Simulation time helper functions (LCM, Euclidean algorithm)
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    private int simTime(ArrayList<Task> tasks, int start, int end) {
+        if ((end - start) == 1) return lcm(tasks.get(start).getP(), tasks.get(end - 1).getP());
+        else return (lcm(tasks.get(start).getP(), simTime(tasks, start + 1, end)));
+    }
+
+    private int lcm(int a, int b) {
+        if (a == 0 || b == 0) {
+            return 0;
+        }
+        return (a * b) / gcd(a, b);
+    }
+
+    private int gcd(int a, int b) {
+        int r = 0;
+        do {
+            r = a % b;
+            a = b;
+            b = r;
+        } while (b != 0);
+        return a;
+    }
+
+
+    private void sort(ArrayList<Task> tasks){
+        for (int i = 1; i < tasks.size(); i++){
+            Task curr = tasks.get(i);
+            for(int j = i - 1; j >= 0; j--){
+                if(curr.getP() > tasks.get(j).getP() && j != i - 1){
+                    Collections.swap(tasks, i, j - 1);
+                }
+            }
+        }
+    }
+
+    private boolean exactTest(ArrayList<Task> tasks){
+        int[] time = new int[tasks.size()];
+
+        for (int i = 0; i < tasks.size(); i++){
+            time[0] += tasks.get(i).getC();
+        }
+        int i = 1;
+        while (true){
+            for (int j = 0; j < tasks.size(); j++){
+                time[i] += tasks.get(j).getC() * (int) Math.ceil(time[i - 1] / tasks.get(j).getP());
+            }
+            if (time[i] > tasks.get(tasks.size() - 1).getP()){
+                return false;
+            } else if (time[i] == time[i - 1]){
+                return true;
+            }
+            i++;
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //Getters of int arrays: id, startTime, endTime, instance
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    public int[] getIdArr() {
+        return idArr;
+    }
+
+    public int[] getStartTimeArr() {
+        return startTimeArr;
+    }
+
+    public int[] getEndTimeArr() {
+        return endTimeArr;
+    }
+
+    public int[] getInstanceArr() {
+        return instanceArr;
     }
 
 }
